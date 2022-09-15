ff5c35305e9676d085e4886c6ec2e65b3e6a8726
silipwn <contact@as-hw.in>
2022-09-15 14:55:33 -0400

    Add img vuln

---
+++ vuln_12.c
@@ -0,0 +1,12 @@
+--2022-09-15 14:55:29--  https://raw.githubusercontent.com/skeeto/integer-overflow-demo/master/render.c
+Loaded CA certificate '/etc/ssl/certs/ca-certificates.crt'
+Resolving raw.githubusercontent.com (raw.githubusercontent.com)... 185.199.111.133, 185.199.108.133, 185.199.109.133, ...
+Connecting to raw.githubusercontent.com (raw.githubusercontent.com)|185.199.111.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 3543 (3.5K) [text/plain]
+Saving to: ‘render.c’
+
+     0K ...                                                   100% 32.0M=0s
+
+2022-09-15 14:55:29 (32.0 MB/s) - ‘render.c’ saved [3543/3543]
+
